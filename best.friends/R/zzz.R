ed2a23bd50e39b4ed5554f2f91fe8d4ce33f91e3
dimalvovs <dmitrijs.lvovs@gmail.com>
2024-06-20 07:43:57 -0400

    rm odd best.friends

--- best.friends/R/zzz.R
+++
@@ -1,4 +0,0 @@
-.onAttach <- function(libname, pkgname) {
-       version <- packageDescription("best.friends", fields="Version")
-       packageStartupMessage(paste("Welcome to best.friends, version", version,"(A Primvera está chegando, e tu?) and stay tuned!"))
-}
