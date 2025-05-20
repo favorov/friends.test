1f14684d8fe03febf0aa3932db249cc9b5c2940b
Alexander Favorov <favorov@sensi.org>
2025-05-20 01:44:31 -0400

    0.99.16

--- R/unif.ks.test.r
+++ R/unif.ks.test.r
@@ -2,19 +2,25 @@
 #' unif.ks.test
 #' 
 #' returns p-value for uniformity of the ranks (they are in \eqn{1 .. N}) vector
-#' #' See [best.friends] documentation for details.
+#' #' See [friends.test] documentation for details.
 #' 
 #' @param ranks vector of ranks of a tag in different collections, \eqn{1 .. N})
+#' @param uniform.max the maximal rank in the uniform, default is NA which means
+#' that we take the max(ranks) as the maximal value
 #' @return p-value for the KS test comparing the ranks distribution with uniform
 #' @importFrom stats ks.test
 #' @examples
 #' example(tag.int.ranks)
 #' ks.p.vals<-apply(TF.ranks,1,"unif.ks.test")
 #' @export
-unif.ks.test<-function(ranks){
+unif.ks.test<-function(ranks,uniform.max=NA){
   jranks<-jitter(ranks,amount=0.1E-6)
   jrmin <- min(jranks)
-  jrmax <- max(jranks)
+  if(is.na(uniform.max)){
+    jrmax <- max(jranks)
+  } else {
+    jrmax<-uniform.max
+  }
   jranks_mapped <- (jranks-jrmin)/(jrmax-jrmin)
   res<-ks.test(jranks_mapped,"punif")
   res$p.value
--- R/zzz.R
+++ R/zzz.R
@@ -1,11 +1,12 @@
 .onAttach <- function(libname, pkgname) {
-    version <- packageDescription("best.friends", fields="Version")
-    packageStartupMessage(paste("Welcome to best.friends, version", version,
-        "(Lebre de mar\u00E7o) and stay tuned!")
+    version <- packageDescription("friends.test", fields="Version")
+    packageStartupMessage(paste("Welcome to friends.test, version", version,
+        "(As f\u00E9rias de maio) and stay tuned!")
     )
 }
 # \u00E1 is á
 # \u00E3 is ã
 # \u00E7 is ç
+# \u00E9 is é
 
 # for detools::check
